bdc12a34be29f1debc9ce7fc3d54266b3adbbb56
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-01 08:36:01 +0100

    typescript tooling, downlevel and other basic

--- test/hello.js
+++ test/hello.js
@@ -1,5 +1,5 @@
 "use strict";
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Maddison", new Date());
