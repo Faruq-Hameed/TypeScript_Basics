dece5b75e70483d4b34203444822915c0ff4596d
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-12 10:55:16 +0100

    learning diff narrowing types

--- test4.ts
+++ test4.ts
@@ -1,8 +1,8 @@
 function padLeft(padding: number | string, input: string) {
     if (typeof padding === "number") {
-      return " ".repeat(padding) + input;
+      return input.repeat(padding) + input;
     }
     return padding + input;
   }
 
-  padLeft(7, '9')
+  console.log(padLeft('7', '9'))
