85b7b9ecb52f992b040db30bb605b874a11954fb
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-16 22:47:34 +0100

    learning function in TS

--- func1.ts
+++ func1.ts
@@ -24,12 +24,58 @@ const passedFunc = () =>{
 
   greeter( passedFunc); //worked but I was expecting it to throw an error
 
+  //funvtion type can also be declared using type alias
   type GreetFunction = (a: string) => void;
 function greeter2(fn: GreetFunction) {
   // ...
 }
-
-greeter2(()=>{
-        console.log('hello world two');
+const rndmFunc = (b: string): string  =>{
+        console.log('hello world');
             return "agg"
-      })
+      }
+const func4 = (num: number)=>{
+    console.log('hello world two');
+  }
+// greeter2(rndmFunc(7)); //thrown error
+
+//call signature
+type DescribableFunction = {
+    description: string;
+    (someArg: number): boolean; //use : to specify the return value type( : boolean)) rather than => ()
+  };
+  function doSomething(fn: DescribableFunction) {
+    console.log(fn.description + " returned " + fn(6));
+  }
+   
+  function myFunc(someArg: number) {
+    return someArg > 3;
+  }
+  myFunc.description = "default description";
+   
+  doSomething(myFunc);
+
+
+  function fn(x: string | number) {
+    if (typeof x === "string") {
+      // do something
+    } else if (typeof x === "number") {
+      // do something else
+    } else {
+      x; // has type 'never'!
+    }
+  }
+//   fn({ x:4})
+
+type voidFunc = () => void;
+ 
+const f1: voidFunc = () => {
+  return '6';
+};
+ 
+const f2: voidFunc = () => true;
+ 
+const f3: voidFunc = function () {
+  return true;
+};
+
+console.log('f1 is ;)', f1);
