3ee539227fc8a007106a11dfb9c203c49e20c396
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-18 21:53:03 +0100

    practicing functions in ts continue

--- functions/test.ts
+++ functions/test.ts
@@ -1,22 +1,29 @@
-import { log } from "console";
-
 function greeter(fn: (a: string) => void): void {
     fn("Hello, World");
   }
    
-  function printToConsole(s: string) : "hello world" {
+  function printToConsole(s: string) : string{
     console.log(s);
-    return 'hello world'
+    return s;
   }
    
   greeter(printToConsole);
   
   type DescribableFunction = {
     description: string;
-    // (someArg: number): boolean;
+    (someArg: number): boolean;
   };
-const obj: DescribableFunction ={
-    description: 'describe'
-}
-  console.log(typeof obj);
-  
+
+  function doSomething(fn: DescribableFunction) {
+    console.log(fn.description + " returned " + fn(6));
+  }
+
+   
+  function myFunc(someArg: number) {
+    return someArg > 3;
+  }
+  myFunc.description = "default description";
+  myFunc.age = 9
+   console.log(myFunc);
+   
+//   doSomething(myFunc);
