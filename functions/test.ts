aa3ac1435fc53b2c400324c18ae4056fce3f5f78
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-19 07:52:22 +0100

    function type expressions

--- functions/test.ts
+++ functions/test.ts
@@ -1,14 +1,24 @@
+// Function Type Expressions
+
 function greeter(fn: (a: string) => void): void {
     fn("Hello, World");
   }
    
   function printToConsole(s: string) : string{
-    console.log(s);
+    console.log({s});
     return s;
   }
    
-  greeter(printToConsole);
+//   greeter(printToConsole);
   
+//   Of course, we can use a type alias to name a function type:
+type GreetFunction = (a: string) => void;
+function greeters(fn: GreetFunction): void {
+ console.log("fn('hello') : ", fn('hello'));
+}
+greeters(printToConsole)
+
+
   type DescribableFunction = {
     description: string;
     (someArg: number): boolean;
@@ -24,6 +34,18 @@ function greeter(fn: (a: string) => void): void {
   }
   myFunc.description = "default description";
   myFunc.age = 9
-   console.log(myFunc);
    
-//   doSomething(myFunc);
+  doSomething(myFunc);
+  console.log({myFunc});
+
+
+type Func = {
+    name: string,
+    (funct:{ age: number, male: boolean}): string
+}
+
+function secondFunc( fn: Func){
+    console.log(`secondFunc called with ${fn.name} and ${fn({ age: 9, male:true})}`);
+}
+
+// secondFunc({age: 1, male: true})
