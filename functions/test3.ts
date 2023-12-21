777347bc1213ac24ad69a8940e9906c82b83e52d
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-21 14:32:29 +0100

    practicing good overloads

--- functions/test3.ts
+++ functions/test3.ts
@@ -24,3 +24,37 @@ function funcSignature(){
 }
 
 console.log(funcSignature('9'))
+
+function fn(x: boolean): void;
+// Argument type isn't right
+function fn(x: string ): void;
+function fn(x: number): boolean;
+// This overload signature is not compatible with its implementation signature.
+function fn(x: string | boolean){ 
+    
+}
+
+//Writing Good Overloads
+
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
+}
+
+// This function is fine; we can invoke it with strings or arrays. 
+// // However, we can’t invoke it with a value that might be a string or an array, 
+// because TypeScript can only resolve a function call to a single overload:
+
+len(""); // OK
+len([0]); // OK
+len(Math.random() > 0.5 ? "hello" : [0]); //throw error
+
+// Because both overloads have the same argument count and same return type, 
+// we can instead write a non-overloaded version of the function:
+
+// function len(x: any[] | string) {
+//   return x.length;
+// }
+// Rule:Always prefer parameters with union types instead of overloads when possible
+
