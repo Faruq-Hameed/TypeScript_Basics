0a06c32f1f5ca07c08e7a604ed45f8581ee0ef1a
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-27 08:32:05 +0100

    added ninja tutorials

---
+++ typescript-tutorial/sandbox.ts
@@ -0,0 +1,12 @@
+const character = 'mario';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
+
+let ninja: object;
+ 
