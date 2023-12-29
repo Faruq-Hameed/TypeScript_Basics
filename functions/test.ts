a1c8f2c6e699c15c0b3fa6f9e36dfe0cf9bbb0b0
Faruq-Hameed <faruqhameed1@gmail.com>
2023-12-29 11:41:37 +0100

    reviwed concept learnt

--- functions/test.ts
+++ functions/test.ts
@@ -54,9 +54,8 @@ secondFunc({name: "faruq", ({age: 1, male: true}{
 })})
 
 type objF = { age: number, male: boolean}
-function objFunc(obj: objF): {
+function objFunc(obj: objF): string;
 
-}
 
 // You can also write a construct signature by adding the new keyword in front of a call signature
 
