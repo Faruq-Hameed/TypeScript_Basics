dd09e6da10d196881ae6d527f0be9d9f20914683
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-01 22:04:23 +0100

    learning TS type template in obj

--- objtec/obj2.ts
+++ objtec/obj2.ts
@@ -12,4 +12,25 @@ var person = {
 // If you use the same code above in Typescript the compiler gives an error. 
 // This is because in Typescript, concrete objects should have a type template. 
 // Objects in Typescript must be an instance of a particular type.
+
 // You can solve this by using a method template in declaration.
+var person2 = {
+    name: 'John',
+    age: 21,
+    greet:function(){ } //type literal
+}
+
+person2.greet = function(){
+    console.log("hello "+person2.name)
+}
+person2.greet()
+
+// var person3 = {
+//     firstName:"Tom", 
+//     lastName:"Hanks", 
+//     sayHello:function() {  }  //Type template 
+//  } 
+//  person3.sayHello = function() {  
+//     console.log("hello "+person3.firstName)
+//  }  
+//  person3.sayHello()
