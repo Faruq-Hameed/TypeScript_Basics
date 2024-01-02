55abd73ad0028ea393bbeb26b5e3e1a90f68be34
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-02 11:43:24 +0100

    practicing TS type template in obj

--- objtec/obj3.ts
+++ objtec/obj3.ts
@@ -35,3 +35,57 @@ const bingoA : Dog = {
     name: 'bingo',
     breed: 'Jack'
 }
+
+
+interface Animal {
+    name: string;
+  }
+   
+  interface Dog extends Animal {
+    breed: string;
+  }
+   
+  // Error: indexing with a numeric string might get you a completely separate type of Animal!
+  interface NotOkay {
+    // [x: number]: Animal; // error
+//   'number' index type 'Animal' is not assignable to 'string' index type 'Dog'.
+    [x: string]: Dog;
+  }
+
+  const objA: NotOkay ={
+    dog: { breed: 'bingo', name: 'luna'}
+  }
+
+  interface NumberDictionary {
+    [index: number]: string;
+   
+    length: number; // ok
+    name: string;// 
+    9: 'abc'
+    // name: number
+//   Property 'name' of type 'string' is not assignable to 'string' index type 'number'.
+  }
+//   In the above example, name’s type does not match the string index’s type, 
+// and the type checker gives an error.
+// However, properties of different types are acceptable 
+// if the index signature is a union of the property types:
+interface NumberDictionaryA {
+    [index: string]: number | string;
+    length: number,
+    name: string // no error
+}
+
+// you can make index signatures readonly in order to prevent assignment to their indices:
+
+interface NumberDictionaryB {
+    readonly[index: number]: string,
+    name: string
+}
+
+var objB : NumberDictionaryB = {
+ 9: "9",
+name: 'faruq'
+}
+
+//ou can make index signatures readonly in order to prevent assignment to their indices:
+// objB[9] = '10' //error
