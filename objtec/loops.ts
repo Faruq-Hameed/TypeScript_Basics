404620427920376fdcc1c99824755f77fedabe36
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-03 22:31:19 +0100

    practicing excess prop check

--- objtec/loops.ts
+++ objtec/loops.ts
@@ -8,8 +8,22 @@ interface IAdd {
 
  type AddFn =  (num1: number, num2:number) => number;
 
- type Jobs = 'salary worker' | 'retired';
- type VIPClient = Jobs & { benefits: string[]};
+//  type Jobs = 'salary worker' | 'retired';
+//  type VIPClient = Jobs & { benefits: string[]};
+// let job: Jobs = "salary worker"
 
- var VIPClient : VIPClient = {"salary worker":55, benefits: ['all', 'hh']}
-// console.log(VIPClient)
+//  let VIPClients : VIPClient = {job, benefits: ['all', 'hh']}
+// // console.log(VIPClient)
+
+interface Client {
+    name: string;
+}
+
+type VIPClient = Client & { benefits: string[]};
+// let client: Client = {
+//     name: "string"
+// }
+
+// Type VIPClient = Client & { benefits: string[]};
+ let VIPClients : VIPClient = {name: "hhh", benefits: ['all', 'hh']}
+
