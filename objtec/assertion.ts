3bbab6302f997b66dd802e26da1710498a994ac1
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-07 17:17:23 +0100

    started generic

--- objtec/assertion.ts
+++ objtec/assertion.ts
@@ -14,8 +14,44 @@ function square(x: number) {
 	return x * x;
 }
 
-const x : number | undefined = square(x) ? x : undefined;
+const x : number | undefined = 7 || undefined;
 
 const answer = square(x)!;
 
 x.toString()
+
+interface Rectangle {
+    width: number,
+    height: number
+}
+
+// function isRectangle(shape: unknown): shape is Rectangle {
+//     if ("width" in shape && "height" in shape) {
+//         // this is a rectangle
+//         return true; 
+//     }
+//     // it's not a rectangle
+//     return false;
+//   }
+
+// using the nullish coalescing operator
+const API_URL_A = process.env.API_URL ?? "DEFAULT URL";
+
+// using the OR (||) logical operator
+const API_URL = process.env.API_URL || "DEFAULT URL";
+
+console.log(API_URL_A);
+
+console.log(9)
+
+// We can also use Javascript Logical Assignment Operator to provide a default value:
+
+let b : string | number;
+
+// provide a default value if null or undefined
+b ??= "Hello World"
+
+// provide a default value if falsy
+b ||= "Hello World"
+console.log(b)
+
