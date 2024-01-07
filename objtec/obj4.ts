21af8c0868ed6c741478f7bfa937b619e00370f5
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-07 18:18:04 +0100

    started generic

--- objtec/obj4.ts
+++ objtec/obj4.ts
@@ -1,5 +1,7 @@
 // Generic Object Types
 
+import { log } from "console";
+
 interface Box {
     contents: unknown;
   }
@@ -8,4 +10,36 @@ interface Box {
     contents: "hello world",
   };
 
-  x.contents = 9
+  // x.contents = 9
+
+  // we could check 'x.contents'
+  if(typeof x.contents === "string"){
+    console.log(x.contents.toUpperCase());
+  }
+
+  // or we could use a type assertion
+  console.log((x.contents as string).toUpperCase(), 'ggggg');
+
+  // One type safe approach would be to instead scaffold out different Box types for every type of contents.
+
+interface NumberBox {
+  contents: number
+}
+
+interface StringBox{
+  contents: string
+}
+
+interface BooleanBox {
+  contents: boolean
+}
+
+// But that means we’ll have to create different functions, or overloads of functions, to operate on these types.
+
+function setContents(box: StringBox, newContents: string): void;
+function setContents(box: BooleanBox, newContents: boolean): void;
+function setContents(box: NumberBox, newContents: number): void;
+function setContents(box: any, newContents: any): void{
+  box.contents = newContents;
+
+}
