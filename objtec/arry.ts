51b381be95584efae1e3b406f2460d36ebaa52a3
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-09 21:57:06 +0100

    started generic array

--- objtec/arry.ts
+++ objtec/arry.ts
@@ -1,1 +1,16 @@
 //The Array Type
+
+// Whenever we write out types like number[] or string[], 
+// that’s really just a shorthand for Array<number> and Array<string>.
+
+interface Box<T>{
+    contents: T
+}
+
+let box: Box<number[]>= {contents: [1, 2,5, ]}
+
+let boxA: Box<Array<number>>={
+    contents: [1, 2, 5]
+}
+
+let boxB: Box<Array<string>> = {contents: ['a', 'b', 6]}
