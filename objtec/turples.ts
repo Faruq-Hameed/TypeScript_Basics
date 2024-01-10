42cc736f7d894203fb9f1a9df0459664fd5ecd27
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-10 09:09:09 +0100

    added turple array

--- objtec/turples.ts
+++ objtec/turples.ts
@@ -1,3 +1,7 @@
 // Tuple Types
 // A tuple type is another sort of Array type that knows exactly how many elements it contains, 
 // and exactly which types it contains at specific positions.
+
+type NumberStringPair = [number, string]
+
+let numStr : NumberStringPair = [3,'three']
