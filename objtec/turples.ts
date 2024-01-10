8ee076383194645b6a6939b6c1e6ad558166d731
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-10 12:35:41 +0100

    added turple array

--- objtec/turples.ts
+++ objtec/turples.ts
@@ -5,3 +5,33 @@
 type NumberStringPair = [number, string]
 
 let numStr : NumberStringPair = [3,'three']
+
+interface NumObj{
+    contents: [number, {}]
+}
+
+let numbObj : NumObj = {contents: [8, {age: 9}]}
+numbObj.contents[1] = {level : 500, graduate: true}
+let ab = numbObj.contents[1] = '9'
+console.log(numbObj.contents)
+//the type literal {} is acting like any
+// Like ReadonlyArray, NumberStringPair has no representation at runtime, but is significant to TypeScript.
+
+// We can also destructure tuples using JavaScript’s array destructuring.
+
+function doStuff (stringHash: [number, string]){
+    const [num, str] = stringHash 
+     
+}
+
+function doSomething(stringHash: [string, number]) {
+    const [inputString, hash] = stringHash;
+   
+    console.log(inputString);
+                    
+  const inputString: string
+   
+    console.log(hash);
+                 
+  const hash: number
+  }
