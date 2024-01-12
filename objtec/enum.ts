884bc4289d9e468244188770cf0393729e3faaab
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-12 07:22:10 +0100

    added enums

--- objtec/enum.ts
+++ objtec/enum.ts
@@ -1,4 +1,4 @@
-In TypeScript, an enum is a way to define a set of named constant values. 
-Enums allow you to define a collection of related values with meaningful names, 
-making your code more readable and maintainable. 
-Enums in TypeScript can be numeric or string-based.
+// In TypeScript, an enum is a way to define a set of named constant values. 
+// Enums allow you to define a collection of related values with meaningful names, 
+// making your code more readable and maintainable. 
+// Enums in TypeScript can be numeric or string-based.
