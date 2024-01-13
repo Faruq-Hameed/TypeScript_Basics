2c76f5afd2a91052feddf8c13867e43e186a24f2
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-13 21:37:09 +0100

    Working with Generic Type Variables

--- generics/gen.ts
+++ generics/gen.ts
@@ -18,3 +18,17 @@ interface Lengthwise {
 loggingIdentity({ length: 10, value: 3 });
 
 //Using Type Parameters in Generic Constraints
+
+You can declare a type parameter that is constrained by another type parameter. 
+For example, here we’d like to get a property from an object given its name. 
+We’d like to ensure that we’re not accidentally grabbing a property that does not exist on the obj, 
+so we’ll place a constraint between the two types:
+
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key];
+}
+ 
+let x = { a: 1, b: 2, c: 3, d: 4 };
+ 
+getProperty(x, "a");
+getProperty(x, "m");
