14b709d854cdd7ac6a195cc42738e96c6a88c1f0
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-13 21:34:31 +0100

    Working with Generic Type Variables

--- generics/gen.ts
+++ generics/gen.ts
@@ -8,4 +8,13 @@ interface Lengthwise {
     console.log(arg.length); // Now we know it has a .length property, so no more error
     return arg;
   }
-  
+//   Because the generic function is now constrained, it will no longer work over any and all types:
+
+// loggingIdentity(3); //error
+// Argument of type 'number' is not assignable to parameter of type 'Lengthwise'.
+
+// Instead, we need to pass in values whose type has all the required properties:
+
+loggingIdentity({ length: 10, value: 3 });
+
+//Using Type Parameters in Generic Constraints
