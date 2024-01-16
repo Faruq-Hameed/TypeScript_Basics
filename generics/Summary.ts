64fef3e42f9f4b96a3f4b9fd6556d843d3f6e758
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-16 14:31:26 +0100

    Working with Generic Type Variables

--- generics/Summary.ts
+++ generics/Summary.ts
@@ -5,6 +5,12 @@
 // Typeof Type Operator - Using the typeof operator to create new types
 // Indexed Access Types - Using Type['a'] syntax to access a subset of a type
 // Conditional Types - Types which act like if statements in the type system
+
+
+
+
+
+
 // Mapped Types - Creating types by mapping each property in an existing type
 // Template Literal Types - Mapped types which change properties via template literal strings
 
