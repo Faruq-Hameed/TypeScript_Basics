b72f20275630974a8e97541edc78e07fd4789de8
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-25 09:14:30 +0100

    learning indexed access ty ts

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -7,4 +7,15 @@ type Age = Person['age']
 // type AgeA = Person[0] //error code, i cannot access the properties by there index like an array
 // type AgeB = Person['age', 'alive'] //error code, only a single property can be accessed
 
+const faruq: Person = {
+    age: 9, name: 'faruq', alive: true
+}
 
+const faruqAge : Age = faruq.age
+
+// The indexing type is itself a type, so we can use unions, keyof, or other types entirely:
+
+type ID = Person[ 'age' | 'name'] //using union type
+
+let faruqId : ID = faruq.age < 20 ? 'twenty plus' : faruq.age
+console.log(typeof faruqId)
