b53ee388feebb9c02a8bed95459541ce24a91df4
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-26 21:36:13 +0100

    started Conditional Types

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -99,6 +99,9 @@ type Example2 = RegExp extends Animal ? number : string // type Example2 = strin
 // (condition ? trueExpression : falseExpression) in JavaScript:
 // SomeType extends OtherType ? TrueType : FalseType;
 
-When the type on the left of the extends is assignable to the one on the right, 
-then you’ll get the type in the first branch (the “true” branch); 
-otherwise you’ll get the type in the latter branch (the “false” branch).
+// When the type on the left of the extends is assignable to the one on the right, 
+// then you’ll get the type in the first branch (the “true” branch); 
+// otherwise you’ll get the type in the latter branch (the “false” branch).
+
+From the examples above, conditional types might not immediately seem useful - we can tell ourselves whether or not Dog extends Animal and pick number or string! 
+But the power of conditional types comes from using them with generics.
