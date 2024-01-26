32283a8671d9d6faad3d6d6b9eb6b2faf2ff51f6
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-26 21:48:39 +0100

    used conditional with function signature

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -123,10 +123,32 @@ function createLabel(nameOrId: string | number): NameLabel | IdLabel {
 throw "unimplemented"
 }
 
-These overloads for createLabel describe a single JavaScript function that 
-makes a choice based on the types of its inputs. Note a few things:
-If a library has to make the same sort of choice over and over throughout its API, 
-this becomes cumbersome. We have to create three overloads: 
-one for each case when we’re sure of the type (one for string and one for number),
-and one for the most general case (taking a string | number). 
-For every new type createLabel can handle, the number of overloads grows exponentially.
+// These overloads for createLabel describe a single JavaScript function that 
+// makes a choice based on the types of its inputs. Note a few things:
+// If a library has to make the same sort of choice over and over throughout its API, 
+// this becomes cumbersome. We have to create three overloads: 
+// one for each case when we’re sure of the type (one for string and one for number),
+// and one for the most general case (taking a string | number). 
+// For every new type createLabel can handle, the number of overloads grows exponentially.
+
+// Instead, we can encode that logic in a conditional type:
+
+type IdOrName<T extends number | string> = T extends number? IdLabel : NameLabel
+
+// We can then use that conditional type to simplify our overloads down to a single function with no overloads.
+
+function createLabelD<T extends number | string>(idOrName: T): NameOrId<T> {
+    throw "unimplemented";
+  }
+   
+  let a = createLabelD("typescript");
+     
+  let a: NameLabel
+   
+  let b = createLabelD(2.8);
+     
+  let b: IdLabel
+   
+  let c = createLabelD(Math.random() ? "hello" : 42);
+  let c: NameLabel | IdLabel
+
