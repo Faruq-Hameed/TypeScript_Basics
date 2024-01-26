519be0f296db7094ce29a44d5be0013fd06a8823
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-26 17:08:57 +0100

    started Conditional Types

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -69,9 +69,15 @@ type KeyB = "age"
 
 type AgeG = Person[KeyB] //type AgeG = number
 
+
+
 //Conditional Types
 
 // At the heart of most useful programs, we have to make decisions based on input. 
 // JavaScript programs are no different, but given the fact that values can be easily introspected, 
 // those decisions are also based on the types of the inputs. 
 // Conditional types help describe the relation between the types of inputs and outputs.
+
+interface Animal {
+    live () : void;
+}
