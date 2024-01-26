540b64cb29803f96ddf350426cb8f9bd3c6b40be
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-26 17:24:57 +0100

    started Conditional Types

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -90,6 +90,7 @@ function alive () : void {
     console.log('i am alive')
 };
 
+
 type Example1 = Dog extends Animal ? number : string; //type Example1 = number
 
 type Example2 = RegExp extends Animal ? number : string // type Example2 = string
