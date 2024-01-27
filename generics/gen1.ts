e36e4cac1e827203c549a01932410401c93677c8
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-27 08:06:58 +0100

    reviwed ts

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -10,13 +10,13 @@ interface Person {
 
 type Age = Person['age']
 // type AgeA = Person[0] //error code, i cannot access the properties by there index like an array
-type AgeB = Person[`${age}`] //error code, only a single property can be accessed
+type AgeB = Person['age'] //error code, only a single property can be accessed
 
 const faruq: Person = {
     age: 9, name: 'faruq', alive: true
 }
 
-const faruqAge : Age = faruq.age
+const faruqAge : Age = undefined | faruq.age 
 
 // The indexing type is itself a type, so we can use unions, keyof, or other types entirely:
 
