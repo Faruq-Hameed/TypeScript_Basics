6885a28dcf86171714eae76c7ba0d0006e38c20a
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-27 08:05:17 +0100

    reviwed ts

--- generics/gen1.ts
+++ generics/gen1.ts
@@ -2,13 +2,11 @@
 // We can use an indexed access type to look up a specific property on another type:
 
 // type Person = {age: number, name: string, alive: boolean}
-
-interface Person{
-age: number,
-name: string
-alive: boolean
+interface Person {
+    age: number | undefined,
+    name: string
+    alive: boolean
 }
-
 
 type Age = Person['age']
 // type AgeA = Person[0] //error code, i cannot access the properties by there index like an array
