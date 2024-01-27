adb592f48b15a165bbb6ef15b051c4480d17063a
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-27 19:57:18 +0100

    added conditional type constraints

---
+++ generics/conditional.ts
@@ -0,0 +1,4 @@
+// Conditional Type Constraints
+// Often, the checks in a conditional type will provide us with some new information. 
+// Just like narrowing with type guards can give us a more specific type, 
+// the true branch of a conditional type will further constrain generics by the type we check against.
