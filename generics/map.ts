d8fd3b17156cad0aac5b5330b9305478f04d498c
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-29 17:49:42 +0100

    code cleaning

--- generics/map.ts
+++ generics/map.ts
@@ -5,10 +5,3 @@ type Properties = 'propA' | 'propB';
 // type M
 
 
-interface NumberDictionary {
-    [index: string]: number;
-   
-    length: number; // ok
-    // name: number;
-//   Property 'name' of type 'string' is not assignable to 'string' index type 'number'.
-  }
