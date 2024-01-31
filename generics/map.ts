d11ced3ed7059cc4882e65fce5911f8d7bbbf3a8
Faruq-Hameed <faruqhameed1@gmail.com>
2024-01-31 10:30:50 +0100

    started mapped types

--- generics/map.ts
+++ generics/map.ts
@@ -1,4 +1,7 @@
 // Map type
+// Mapped types are a powerful feature of TypeScript 
+// that allows you to create new types by transforming the properties of an existing type. 
+
 
 type Properties = 'propA' | 'propB';
 
