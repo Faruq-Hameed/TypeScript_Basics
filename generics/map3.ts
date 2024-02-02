1a9ceb57a3f8c015c81fb51fd8b661cb9d071863
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-02 21:23:49 +0100

    started temperate literal types

--- generics/map3.ts
+++ generics/map3.ts
@@ -1,7 +1,24 @@
 // // Template Literal Types;
 
-// Template literal types build on string literal types, 
-// and have the ability to expand into many strings via unions.
-// They have the same syntax as template literal strings in JavaScript, 
-// but are used in type positions. When used with concrete literal types, 
-// a template literal produces a new string literal type by concatenating the contents.
+ /** Template literal types build on string literal types, 
+ and have the ability to expand into many strings via unions.
+ They have the same syntax as template literal strings in JavaScript, 
+ but are used in type positions. When used with concrete literal types, 
+ a template literal produces a new string literal type by concatenating the contents. */
+
+type World = 'world';
+/**type Greetings = "hello world" */
+type Greetings = `hello ${World}`
+
+/*When a union is used in the interpolated position, 
+the type is the set of every possible string literal that could be represented by each union member: */
+
+type EmailLocalIDs = 'welcome' | 'heading'
+type FooterLocaleIDs = "footer" | "sendoff";
+
+/**type AllLocalIDs = "welcome_id" | "heading_id" | "footer_id" | "sendoff_id" */
+type AllLocalIDs = `${EmailLocalIDs | FooterLocaleIDs}_id`; //_id will be concatenated with each one
+
+let localIDs : AllLocalIDs = 'heading_id'
+// let localIDs2 : AllLocalIDs = 'heading' //error code
+
